ee9cedeb54f83739c8d9ac1cbe852b36e9a05dda
CodeBlue0 <piri007@cau.ac.kr>
2023-01-04 14:30:55 +0900

    Chapter #32

--- run.sh
+++ run.sh
@@ -12,7 +12,10 @@
 # qemu-system-x86_64 -L . -m 64 -fda ./Disk.img -hda ./HDD.img -boot a -M pc -serial tcp::4444,server,nowait -smp 2,sockets=2
 
 # Multi Core -16
-qemu-system-x86_64 -L . -m 64 -fda ./Disk.img -hda ./HDD.img -boot a -M pc -serial tcp::4444,server,nowait -smp 16,sockets=16
+# qemu-system-x86_64 -L . -m 64 -fda ./Disk.img -hda ./HDD.img -boot a -M pc -serial tcp::4444,server,nowait -smp 16,sockets=16
+
+# Multi Core -16, RAM
+qemu-system-x86_64 -L . -m 64 -fda ./Disk.img -boot a -M pc -serial tcp::4444,server,nowait -smp 16,sockets=16
 
 ### for debug
 ###qemu-system-x86_64 -L . -m 64 -fda ./Disk.img -M pc -S -s
