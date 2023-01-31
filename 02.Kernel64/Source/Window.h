d053aa9886760864a02653d7ab5537b23891c6c9
CodeBlue0 <piri007@cau.ac.kr>
2023-01-31 12:07:43 +0900

    Chapter #45

--- 02.Kernel64/Source/Window.h
+++ 02.Kernel64/Source/Window.h
@@ -318,4 +318,9 @@ BOOL kGetStartPositionInDrawBitmap(const DRAWBITMAP* pstDrawBitmap,
     int iX, int iY, int* piByteOffset, int* piBitOffset);
 BOOL kIsDrawBitmapAllOff(const DRAWBITMAP* pstDrawBitmap);
 
+BOOL kBitBlt(QWORD qwWindowID, int iX, int iY, COLOR* pstBuffer, int iWidth,
+    int iHeight);
+void kDrawBackgroundImage(void);
+
+
 #endif /*__WINDOW_H__*/
